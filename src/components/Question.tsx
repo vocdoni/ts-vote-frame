5e6b906cf9720c9c792adc46584e577a3fd9265f
Òscar Casajuana <elboletaire@gmail.com>
2024-03-06 19:43:54 +0100

    Frame style improvements
    - reduced progress bar length, but increased size, reducing render time
      slightly
    - redistributed a bit the results frame so it shows more spaced when
      less choices are shown

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -32,14 +32,19 @@ export const Results = ({ election }: { election: Partial<PublishedElection> }) 
     3: [3, 7],
     4: [3, 6],
   }
-  const [min, max] = sizes[election.questions[0].choices.length] || [2, 5]
+  const [min, max] = sizes[election.questions[0].choices.length] || [4, 7]
   const tfs = calculateFontSize(election.title.default.length, min, max, 250)
 
   const [question] = election.questions
   // get choices length
   const clength = question.choices.reduce((prev, curr) => prev + curr.title.default.length, 0)
-  // reuse the same sizes from the title to be used as choices font-size
-  const cfs = calculateFontSize(clength, min, max, 200)
+  const rsizes = {
+    2: [4, 6],
+    3: [3, 6],
+    4: [3, 5],
+  }
+  const [rmin, rmax] = rsizes[election.questions[0].choices.length] || [4, 6]
+  const cfs = calculateFontSize(clength, rmin, rmax, 200)
 
   const [results] = election.results
   // map results to numbers
@@ -49,32 +54,38 @@ export const Results = ({ election }: { election: Partial<PublishedElection> }) 
 
   return (
     <Layout icon='📊'>
-      <div tw={`text-${tfs}xl`}>{election.title.default}</div>
-      {weight <= 0 ? (
-        <p tw='text-6xl'>No results yet</p>
-      ) : (
-        <>
-          <div tw={`text-${tfs - 1}xl mb-6 flex`}>
+      <div tw='flex flex-col grow'>
+        <div tw={`text-${tfs}xl`}>{election.title.default}</div>
+        {weight > 0 && (
+          <div tw={`text-${tfs - 1}xl mt-2 mb-4 flex`}>
             Votes: {election.voteCount}
             <Turnout election={election} /> | Weight: {weight}
           </div>
-          <ul tw={`text-${cfs}xl flex-col max-w-screen-sm mr-30`}>
-            {question.choices.map((choice, i) => {
-              const percent = Math.round((rnum[i] / weight) * 1000) / 10
-              return (
-                <li tw='flex-col'>
-                  <span>
-                    {i + 1}. {choice.title.default}
-                  </span>
-                  <span tw='text-2xl'>
-                    {generateProgressBar(percent)} {percent}%
-                  </span>
-                </li>
-              )
-            })}
-          </ul>
-        </>
-      )}
+        )}
+      </div>
+      <div tw='flex flex-col grow'>
+        {weight <= 0 ? (
+          <p tw='text-6xl'>No results yet</p>
+        ) : (
+          <>
+            <ul tw={`text-${cfs}xl flex-col max-w-screen-sm mr-30`}>
+              {question.choices.map((choice, i) => {
+                const percent = Math.round((rnum[i] / weight) * 1000) / 10
+                return (
+                  <li tw='flex-col'>
+                    <span>
+                      {i + 1}. {choice.title.default}
+                    </span>
+                    <span tw='text-4xl'>
+                      {generateProgressBar(percent)} {percent}%
+                    </span>
+                  </li>
+                )
+              })}
+            </ul>
+          </>
+        )}
+      </div>
     </Layout>
   )
 }
@@ -101,7 +112,7 @@ export const calculateFontSize = (
 }
 
 const generateProgressBar = (percent: number): string => {
-  const maxBarLength = 20
+  const maxBarLength = 14
   const length = (percent * maxBarLength) / 100
   const bar = '🟪'.repeat(length)
   const empty = '⬜'.repeat(maxBarLength - length)
